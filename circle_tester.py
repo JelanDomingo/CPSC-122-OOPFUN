2e373afc2df84aeb4fc003ec81e60057fa3602c1
JelanDomingo <jrcd2006@gmail.com>
2026-02-03 13:43:35 -0800

    Added str representation for Class Circle

--- circle_tester.py
+++ circle_tester.py
@@ -17,9 +17,31 @@ class Point:
         """
         self._x = new_x
 
-    
+class Circle:
+    """Represents a simple circle.
+    """
+    def __init__(self, radius: float = 1.0, center: Point = None) -> None:
+        self._radius = radius
+        if center is None:
+            center = Point()
+            self._center = center
+        self._center = center
+        pass
+
+    def __str__(self) -> str:
+        """
+        Docstring for __str__
+        
+        :param self: Description
+        """
+        return f"Radius: {self._radius} \nCenter: {self._center}"
 
 origin = Point()
 print(origin)
 p1 = Point(-4.2, 7.1)
 print(p1)
+
+unit_circle = Circle()
+print(unit_circle)
+c1 = Circle(5.0, p1)
+print(c1)
